efa7947f15b974646b4e3231da1e797d68b2ad66
StevenRice99 <Contact@StevenRice.ca>
2024-09-23 09:37:07 -0400

    Added indexing for how often terms occur in the entire text of a paper.

--- src/main/java/ca/uwindsor/common/Constants.java
+++ src/main/java/ca/uwindsor/common/Constants.java
@@ -1,5 +1,10 @@
 package ca.uwindsor.common;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+
 /**
  * Store constants for the project.
  */
@@ -25,4 +30,33 @@ public class Constants {
      * This can be used to help capture author names or keywords for instance.
      */
     public static final int indexedLines = 10;
+
+    /**
+     * Store the computer science terms.
+     */
+    private static HashSet<String> terms = null;
+
+    /**
+     * Get the computer science terms.
+     * @return The computer science terms.
+     * @throws IOException If there is an error loading the computer science terms.
+     */
+    public static HashSet<String> getTerms() throws IOException
+    {
+        // If the terms have not yet been loaded, load them.
+        if (terms == null)
+        {
+            terms = new HashSet<>();
+            BufferedReader br = new BufferedReader(new FileReader(keyTerms));
+            // Read the file, with a new term on each line.
+            String line;
+            while ((line = br.readLine()) != null)
+            {
+                terms.add(line.trim().toLowerCase());
+            }
+            br.close();
+        }
+
+        return terms;
+    }
 }
--- src/main/java/ca/uwindsor/indexing/ComputerScienceAnalyzer.java
+++ src/main/java/ca/uwindsor/indexing/ComputerScienceAnalyzer.java
@@ -6,8 +6,6 @@ import org.apache.lucene.analysis.synonym.SynonymGraphFilter;
 import org.apache.lucene.analysis.synonym.SynonymMap;
 import org.apache.lucene.util.CharsRef;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 
 import ca.uwindsor.common.Constants;
@@ -15,7 +13,8 @@ import ca.uwindsor.common.Constants;
 /**
  * Allow for ensuring we capture computer science terms as single tokens.
  */
-public class ComputerScienceAnalyzer extends Analyzer {
+public class ComputerScienceAnalyzer extends Analyzer
+{
     /**
      * The map of all computer science terms we want to keep as singular tokens.
      */
@@ -23,21 +22,17 @@ public class ComputerScienceAnalyzer extends Analyzer {
 
     /**
      * Load the computer science terms.
-     * 
      * @throws IOException If there is an error loading the computer science terms.
      */
-    public ComputerScienceAnalyzer() throws IOException {
+    public ComputerScienceAnalyzer() throws IOException
+    {
         // Create the builder for the terms.
         SynonymMap.Builder builder = new SynonymMap.Builder(true);
         // Open the file containing the terms.
-        BufferedReader br = new BufferedReader(new FileReader(Constants.keyTerms));
-        // Read the file, with a new term on each line.
-        String line;
-        while ((line = br.readLine()) != null) {
-            String trimmed = line.trim();
-            builder.add(new CharsRef(trimmed.replaceAll(" ", "_")), new CharsRef(trimmed), true);
+        for (String term : Constants.getTerms())
+        {
+            builder.add(new CharsRef(term.replaceAll(" ", "_")), new CharsRef(term), true);
         }
-        br.close();
         // Save the map for use.
         this.synonymMap = builder.build();
     }
@@ -50,7 +45,8 @@ public class ComputerScienceAnalyzer extends Analyzer {
      * @return The tokens.
      */
     @Override
-    protected TokenStreamComponents createComponents(String fieldName) {
+    protected TokenStreamComponents createComponents(String fieldName)
+    {
         // Use the standard tokenizer as a base.
         StandardTokenizer src = new StandardTokenizer();
         // Add in our key terms to be tokenized.
