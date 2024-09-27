8bb9c91d1d51876c1636d53b95d25b0d24817f3b
Matthew Belanger <matt_belanger1@outlook.com>
2024-09-27 17:42:53 -0400

    using constant naming conventions

--- src/main/java/ca/uwindsor/common/Constants.java
+++ src/main/java/ca/uwindsor/common/Constants.java
@@ -8,35 +8,35 @@ public class Constants
     /**
      * The name of the data to use.
      */
-    public static final String data = "citeceer2";
+    public static final String DATA = "citeceer2";
 
     /**
      * The data index folder.
      */
-    public static final String dataIndex = data + "_index";
+    public static final String DATA_INDEX = DATA + "_index";
 
     /**
      * The file containing key computer science terms we wish to tokenize with.
      */
-    public static final String keyTerms = "terms.txt";
+    public static final String KEY_TERMS = "terms.txt";
 
     /**
      * The name of the field path.
      */
-    public static final String FieldPath = "path";
+    public static final String FIELD_PATH = "path";
 
     /**
      * The name of the title path.
      */
-    public static final String FieldTitle = "title";
+    public static final String FIELD_TITLE = "title";
 
     /**
      * The name of the content's path.
      */
-    public static final String FieldContents = "contents";
+    public static final String FIELD_CONTENTS = "contents";
 
     /**
      * The name of the keyword's path.
      */
-    public static final String FieldKeywords = "keywords";
+    public static final String FIELD_KEYWORDS = "keywords";
 }
