24955702affb5648b67c108bb40fdf5bb540da61
StevenRice99 <Contact@StevenRice.ca>
2024-09-26 21:45:11 -0400

    Can now index on and search by key terms. The key terms can be either single or multi-words.

---
+++ src/main/java/ca/uwindsor/analyzing/KeyTermsFilter.java
@@ -0,0 +1,151 @@
+package ca.uwindsor.analyzing;
+
+import ca.uwindsor.common.Constants;
+import org.apache.lucene.analysis.TokenFilter;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+
+/**
+ * Filter for key terms.
+ */
+public class KeyTermsFilter extends TokenFilter
+{
+    /**
+     * The terms to match.
+     */
+    private static HashSet<String> terms;
+
+    /**
+     * Required value for the tokenizing.
+     */
+    private final CharTermAttribute termAttr = addAttribute(CharTermAttribute.class);
+
+    /**
+     * Buffer for building term pairs.
+     */
+    private final Deque<String> buffer = new ArrayDeque<>();
+
+    /**
+     * Create the filter.
+     */
+    public KeyTermsFilter(TokenStream input)
+    {
+        super(input);
+
+        // If the terms have not yet been loaded, load them.
+        if (terms != null)
+        {
+            return;
+        }
+
+        // Define our custom terms.
+        terms = new HashSet<>();
+
+        try
+        {
+            BufferedReader br = new BufferedReader(new FileReader(Constants.keyTerms));
+            // Read the file, with a new term on each line.
+            String line;
+            while ((line = br.readLine()) != null)
+            {
+                terms.add(line.trim().toLowerCase());
+            }
+            br.close();
+        } catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * See if we should increment the number of tokens.
+     * @return True if a key term was matched, false otherwise.
+     * @throws IOException Reading the input fails.
+     */
+    @Override
+    public boolean incrementToken() throws IOException
+    {
+        // Loop until we have a match or are at the end of the file.
+        while (true)
+        {
+            // If we have nothing in the buffer, load the next term.
+            if (buffer.isEmpty())
+            {
+                // If there are no more tokens to load we are done.
+                if (!input.incrementToken())
+                {
+                    return false;
+                }
+
+                // Add the term to our buffer.
+                buffer.addLast(termAttr.toString());
+            }
+
+            // Combine any buffered phrases.
+            String currentPhrase = String.join(" ", buffer);
+
+            // Check if this is a term we wish to match.
+            if (terms.contains(currentPhrase))
+            {
+                termAttr.setEmpty().append(currentPhrase);
+                buffer.clear();
+                return true;
+            }
+
+            // Check if there is any way this buffer could be a term.
+            if (!isPotentialTerm(currentPhrase))
+            {
+                // This is not a match and no potential for it to be part of a larger one so clear it.
+                buffer.removeFirst();
+                continue;
+            }
+
+            // Load the next token if this could be a term.
+            if (input.incrementToken())
+            {
+                buffer.addLast(termAttr.toString());
+                continue;
+            }
+
+            // Otherwise, there are no more tokens so stop.
+            buffer.clear();
+            return false;
+        }
+    }
+
+    /**
+     * Check if there is any potential the current buffer could be a term.
+     * @param currentPhrase The current phrase.
+     * @return True if there is potential, false otherwise.
+     */
+    private boolean isPotentialTerm(String currentPhrase)
+    {
+        for (String phrase : terms)
+        {
+            if (phrase.startsWith(currentPhrase))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Reset the buffer.
+     * @throws IOException Any errors from resetting.
+     */
+    @Override
+    public void reset() throws IOException
+    {
+        super.reset();
+        buffer.clear();
+    }
+}
--- src/main/java/ca/uwindsor/searching/Searcher.java
+++ src/main/java/ca/uwindsor/searching/Searcher.java
@@ -1,48 +1,152 @@
 package ca.uwindsor.searching;
 
+import java.io.IOException;
 import java.nio.file.Paths;
 
-import ca.uwindsor.analyzing.ComputerScienceAnalyzer;
+import ca.uwindsor.analyzing.KeyTermsAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.*;
+import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.*;
 import org.apache.lucene.store.FSDirectory;
 
 import ca.uwindsor.common.Constants;
+import org.apache.lucene.util.BytesRef;
 
 /**
- * Simplified Lucene Search .
+ * Simplified Lucene Search.
  * Based on code by Jianguo Lu.
  */
 public class Searcher
 {
+	/**
+	 * Run some searching.
+	 * @param args For now, these are not used and the request is hardcoded.
+	 * @throws Exception Error reading from the index.
+	 */
 	public static void main(String[] args) throws Exception
 	{
+		// For now, our test strings are hardcoded.
+		String request = "artificial intelligence and machine learning concepts";
+
 		// Load the indexed files.
 		IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(Constants.dataIndex)));
 		IndexSearcher searcher = new IndexSearcher(reader);
-		// Use our custom analyzer.
-		QueryParser parser = new QueryParser("contents", new ComputerScienceAnalyzer(false));
-		// For now, this is a hardcoded query.
-		Query query = parser.parse("information OR retrieval");
+
 		// Get the top five results.
-		TopDocs results = searcher.search(query, 5);
+		// Swap the method for which way we want to search.
+		// The options are "ContentsSearch", "KeywordsSearch", or "CombinedSearch".
+		// Eventually, we can try doing a method which uses one metric and then another as a fallback.
+		// For instance, maybe use keywords only to start, and if nothing/little returns we can do the rest standard.
+		TopDocs results = KeywordsSearch(searcher, request, 5);
+
+		// Output details of the files we returned.
 		System.out.println(results.totalHits + " total matching documents");
 		for (int i = 0; i < 5 && i < results.scoreDocs.length; i++)
 		{
+			// We should look into other methods to avoid deprecation, but everything I read say to use this...
 			@SuppressWarnings("deprecation")
 			Document doc = searcher.doc(results.scoreDocs[i].doc);
-			String path = doc.get("path");
-			System.out.println((i + 1) + ". " + path);
+
+			// Write the path to the file.
+			System.out.println((i + 1) + ". " + doc.get("path"));
+
+			// Write the title.
 			String title = doc.get("title");
-			if (title != null) {
+			if (title != null)
+			{
 				System.out.println("   Title: " + doc.get("title"));
 			}
+
+			// Get how many times keywords are in a document for seeing how the matching is working.
+			@SuppressWarnings("deprecation")
+			Terms terms = reader.getTermVector(results.scoreDocs[i].doc, Constants.FieldKeywords);
+			if (terms != null)
+			{
+				// Access the terms for this field.
+				TermsEnum termsEnum = terms.iterator();
+
+				// Iterate through all terms.
+				BytesRef term;
+				while ((term = termsEnum.next()) != null)
+				{
+					// Get the frequency of the term in the document.
+					System.out.println("   " + term.utf8ToString() + " = " + (int) termsEnum.totalTermFreq());
+				}
+			}
 		}
+
+		// Cleanup the reader.
 		reader.close();
+	}
+
+	/**
+	 * Run a combined search using both the contents and the keywords.
+	 * @param searcher The searcher.
+	 * @param request The request we want to search for.
+	 * @param number The number of results we want.
+	 * @return The top results matching the request.
+	 * @throws IOException Error reading the indexed documents.
+	 * @throws ParseException Error parsing the requested search.
+	 */
+	private static TopDocs CombinedSearch(IndexSearcher searcher, String request, int number) throws IOException, ParseException
+    {
+		// Add both searches, treating them similar to a logical "OR".
+		BooleanQuery.Builder builder = new BooleanQuery.Builder();
+		builder.add(ContentsQuery(request), BooleanClause.Occur.SHOULD);
+		builder.add(KeywordsQuery(request), BooleanClause.Occur.SHOULD);
+		return searcher.search(builder.build(), number);
+	}
+
+	/**
+	 * Run a search using the contents.
+	 * @param searcher The searcher.
+	 * @param request The request we want to search for.
+	 * @param number The number of results we want.
+	 * @return The top results matching the request.
+	 * @throws IOException Error reading the indexed documents.
+	 * @throws ParseException Error parsing the requested search.
+	 */
+	private static TopDocs ContentsSearch(IndexSearcher searcher, String request, int number) throws IOException, ParseException
+    {
+		return searcher.search(ContentsQuery(request), number);
+	}
+
+	/**
+	 * Run a search using the keywords.
+	 * @param searcher The searcher.
+	 * @param request The request we want to search for.
+	 * @param number The number of results we want.
+	 * @return The top results matching the request.
+	 * @throws IOException Error reading the indexed documents.
+	 * @throws ParseException Error parsing the requested search.
+	 */
+	private static TopDocs KeywordsSearch(IndexSearcher searcher, String request, int number) throws IOException, ParseException
+	{
+		return searcher.search(KeywordsQuery(request), number);
+	}
+
+	/**
+	 * Create a query for the contents.
+	 * @param request The request we want to search for.
+	 * @return The query.
+	 * @throws ParseException Error parsing the requested search.
+	 */
+	private static Query ContentsQuery(String request) throws ParseException
+    {
+		return new QueryParser(Constants.FieldContents, new StandardAnalyzer()).parse(request);
+	}
+
+	/**
+	 * Create a query for the keywords.
+	 * @param request The request we want to search for.
+	 * @return The query.
+	 * @throws ParseException Error parsing the requested search.
+	 */
+	private static Query KeywordsQuery(String request) throws ParseException
+	{
+		return new QueryParser(Constants.FieldKeywords, new KeyTermsAnalyzer()).parse(request);
 	}
 }
